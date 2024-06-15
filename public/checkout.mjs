8907c7b8291e2462dfbe3d0bd535e30ee8178084
jonas <gestopajonas@gmail.com>
2024-06-15 08:41:21 +0800

    add: placeholder function

--- public/checkout.mjs
+++ public/checkout.mjs
@@ -20,3 +20,22 @@ cartItems.map((cartItem) => {
 
   cartItemsContainer.appendChild(form);
 });
+
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const phone = document.getElementById('phone');
+const address = document.getElementById('address');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const deliveryInputs = { name: name.value, phone: phone.value, address: address.value }
+  console.log(deliveryInputs);
+  saveToLocalStorage(deliveryInputs);
+  alert("Your order has been successfully placed! Thank you for shopping with us.")
+  window.location.href = 'http://localhost:3000/';
+});
+
+function saveToLocalStorage(inputs) {
+  localStorage.setItem('deliveryInfo', JSON.stringify(inputs))
+}
