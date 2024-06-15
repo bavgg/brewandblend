290659957356edae850ff64ec06f0de75e376c23
jonas <gestopajonas@gmail.com>
2024-06-15 09:09:50 +0800

    update: placeOrder function

--- public/checkout.mjs
+++ public/checkout.mjs
@@ -10,14 +10,10 @@ cartItems.map((cartItem) => {
   form.innerHTML = `
     <img  width="100" src="${cartItem.image}">
     <h4>${cartItem.title}</h4>
-
     <p>Price: $${cartItem.price}</p>
-
     <p>Quantity: ${cartItem.quantity}</p>
-
     <p>Subtotal: $${cartItem.quantity * cartItem.price}</p>
   `;
-
   cartItemsContainer.appendChild(form);
 });
 
@@ -26,11 +22,18 @@ const name = document.getElementById('name');
 const phone = document.getElementById('phone');
 const address = document.getElementById('address');
 
+if (getDeliveryInfoFromLocalstorage() !== null) {
+  const infoDelivery = getDeliveryInfoFromLocalstorage();
+  name.value = infoDelivery.name;
+  phone.value = infoDelivery.phone;
+  address.value = infoDelivery.address;
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const deliveryInputs = { name: name.value, phone: phone.value, address: address.value }
-  console.log(deliveryInputs);
+
   saveToLocalStorage(deliveryInputs);
   alert("Your order has been successfully placed! Thank you for shopping with us.")
   window.location.href = 'http://localhost:3000/';
@@ -38,4 +41,14 @@ form.addEventListener('submit', (e) => {
 
 function saveToLocalStorage(inputs) {
   localStorage.setItem('deliveryInfo', JSON.stringify(inputs))
+}
+
+function getDeliveryInfoFromLocalstorage() {
+  let deliveryInfo = localStorage.getItem('deliveryInfo');
+  if (deliveryInfo !== null) {
+    deliveryInfo = JSON.parse(deliveryInfo);
+    return deliveryInfo;
+  } else {
+    return null;
+  }
 }
